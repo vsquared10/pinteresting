0d7f4872a03d8d2310b18a1f6bff0015c8a52f71
Victor Vulovic <victorvulovic@gmail.com>
2015-02-23 01:11:25 -0800

    attempt to adjust settings to upload content to heroku

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Pinteresting
 
     # required for heroku
     config.assets.initialize_on_precompile = false
+    
+    # attempt to debug heroku content upload
+    config.serve_static_assets = true
   end
 end
