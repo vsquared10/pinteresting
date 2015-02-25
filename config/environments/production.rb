119c9d9ed1f251a922a7d919c37390b7b5a183b7
Victor Vulovic <victorvulovic@gmail.com>
2015-02-24 17:10:18 -0800

    tweaked all files to match final git file from Mattan

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+Pinteresting::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-Rails.application.routes.draw do
-  devise_for :users
+Pinteresting::Application.routes.draw do
   resources :pins
 
+  devise_for :users
   root "pins#index"
   get "about" => "pages#about" # creates about_path
 
